c343784974d429f98883a357969229ceaca0de41
guilherme munis <gmmunis@hotmail.com>
2020-11-25 13:02:08 -0300

    Relacionamento nos models

--- backend/src/database/migrations/1606315942628-CreateUsers.ts
+++ backend/src/database/migrations/1606315942628-CreateUsers.ts
@@ -9,7 +9,7 @@ export default class CreateUsers1606315942628 implements MigrationInterface {
                 columns: [
                     {
                         name: 'id',
-                        type: 'varchar',
+                        type: 'uuid',
                         isPrimary: true,
                         generationStrategy: 'uuid',
                         default: 'uuid_generate_v4()',

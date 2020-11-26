35528e5365c8406497ed62e66ce1d38bc8aab633
guilherme munis <gmmunis@hotmail.com>
2020-11-26 17:33:49 -0300

    Estilos globais

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
 
-const App: React.FC = () => <h1>Hello</h1>;
 
-
+const App: React.FC = () => (
+  <>
+    <h1>Hello Stack</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;

9cee3775644010ff8b13fa56592f6d035581fb48
guilherme munis <gmmunis@hotmail.com>
2020-12-02 11:24:52 -0300

    Integrando Unform

--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
-
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -18,6 +19,7 @@ import {
 
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
   const navigation = useNavigation();
 
   return (
@@ -37,13 +39,18 @@ const SignUp: React.FC = () => {
             <View>
               <Title>Crie sua conta</Title>
             </View>
+
+            <Form ref={formRef} onSubmit={(data) => {
+              console.log(data);
+            }}>
             <Input name="name" icon="user" placeholder="Nome" />
 
             <Input name="email" icon="mail" placeholder="E-mail" />
 
             <Input name="password" icon="lock" placeholder="Senha" />
 
-            <Button onPress={() => { }}>Entrar</Button>
+            <Button onPress={() => formRef.current?.submitForm()}>Entrar</Button>
+            </Form>
 
           </Container>
         </ScrollView>

6116a055081ea6c7cef0b5fec76191c2b227139d
guilherme munis <gmmunis@hotmail.com>
2020-12-05 11:36:16 -0300

    Injeção de dependências

--- backend/src/modules/users/infra/http/routes/users.routes.ts
+++ backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
+import { container } from 'tsyringe';
 
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+
 import CreateUserService from '@modules/users/services/CreateUserService';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
@@ -15,8 +16,7 @@ usersRouter.post('/', async (request, response) => {
 
   const { name, email, password } = request.body;
 
-  const usersRepository = new UsersRepository();
-  const createUser = new CreateUserService(usersRepository);
+  const createUser = container.resolve(CreateUserService);
 
   const user = await createUser.execute({
     name,
@@ -41,8 +41,7 @@ usersRouter.patch(
   upload.single('avatar'),
   async (request, response) => {
 
-    const usersRepository = new UsersRepository();
-    const updateUserAvatar = new UpdateUserAvatarService(usersRepository);
+    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
 
     const user = await updateUserAvatar.execute({
       user_id: request.user.id,

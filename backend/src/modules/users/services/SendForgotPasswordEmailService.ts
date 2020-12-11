c91a30ff6ad2acef2a5a147e54f510f8185ddeba
guilherme munis <gmmunis@hotmail.com>
2020-12-11 13:08:02 -0300

    Template de emails

--- backend/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -19,7 +19,7 @@ class SendForgotPasswordEmailService {
     private mailProvider: IMailProvider,
     
     @inject('UserTokensRepository')
-    private userTokensRepository: IUserTokensRepository, ) { }
+    private userTokensRepository: IUserTokensRepository, ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
     const user = await this.usersRepository.findByEmail(email);
@@ -30,10 +30,20 @@ class SendForgotPasswordEmailService {
 
     const {token} = await this.userTokensRepository.generate(user.id);
 
-    await this.mailProvider.sendMail(
-      email,
-      `Pedido de recuperação de senha recebido: ${token}`,
-    );
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[GoBarber] Recuperação de senha',
+      templateData: {
+        template: 'Olá, {{name}}: {{token}}',
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
+    });
   }
 }
 

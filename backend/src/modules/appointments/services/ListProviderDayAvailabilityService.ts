dc484bfdab876912b5c0f6a1296dba6390e764c0
guilherme munis <gmmunis@hotmail.com>
2020-12-15 16:27:51 -0300

    Excluindo horários antigos

--- backend/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ backend/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,4 +1,4 @@
-import { getHours } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 // import { getDate, getDaysInMonth } from 'date-fns';
 
@@ -41,14 +41,18 @@ class ListProviderDayAvailabilityService {
       (_, index) => index + hourStart,
     );
 
+    const currentDate = new Date(Date.now());
+
     const availability = eachHourArray.map(hour => {
       const hasAppointmentInHour = appointments.find(appointment =>
         getHours(appointment.date) === hour,
       );
 
+      const compareDate = new Date(year, month - 1, day, hour);
+
       return {
         hour,
-        available: !hasAppointmentInHour,
+        available: !hasAppointmentInHour && isAfter(compareDate, currentDate),
       };
     });
 

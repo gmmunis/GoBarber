e78fa1199ed0e286f42a86b25bebc34079e50cca
guilherme munis <gmmunis@hotmail.com>
2020-12-17 12:23:45 -0300

    Utilizando Class Transformer

--- backend/src/modules/users/infra/http/controllers/ProfileController.ts
+++ backend/src/modules/users/infra/http/controllers/ProfileController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import UpdateProfileService from '@modules/users/services/UpdateProfileServise';
 import ShowProfileService from '@modules/users/services/ShowProfileService';
@@ -13,9 +14,8 @@ export default class ProfileController {
 
     const user = await showProfile.execute({ user_id });
 
-    delete user.password;
 
-    return response.json(user);
+    return response.json(classToClass(user));
   }
   
   public async update(request: Request, response: Response): Promise<Response> {
@@ -32,14 +32,6 @@ export default class ProfileController {
       password,
     });
 
-    const userWithoutPassword = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    };
-
-    return response.json(userWithoutPassword);
+    return response.json(classToClass(user));
   }
 }

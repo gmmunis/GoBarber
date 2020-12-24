4e45abd0631becb714ba93459df88a129e3bd85b
guilherme munis <gmmunis@hotmail.com>
2020-12-24 12:25:39 -0300

    finalizando listagem de agendamentos e criando a página de perfil

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import { isToday, format, parseISO  } from 'date-fns';
+import { isToday, format, parseISO, isAfter } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import DayPicker, { DayModifiers } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
@@ -21,6 +21,7 @@ import {
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
 
 interface MonthAvailabilityItem {
   day: number;
@@ -44,10 +45,10 @@ const Dashboard: React.FC = () => {
 
   const [monthAvailability, setMonthAvailability] = useState<MonthAvailabilityItem[]>([]);
 
-  const [appointments,setAppointments] = useState<AppointmentCalendar[]>([]);
+  const [appointments, setAppointments] = useState<AppointmentCalendar[]>([]);
 
   const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
-    if (modifiers.available) {
+    if (modifiers.available && !modifiers.disabled) {
       setSelectedDate(day);
     }
   }, []);
@@ -90,12 +91,12 @@ const Dashboard: React.FC = () => {
 
   const disabledDays = useMemo(() => {
     const dates = monthAvailability.filter(monthDay => monthDay.available === false)
-    .map(monthDay => {
-      const year = currentMonth.getFullYear();
-      const month = currentMonth.getMonth();
+      .map(monthDay => {
+        const year = currentMonth.getFullYear();
+        const month = currentMonth.getMonth();
 
-      return new Date(year, month, monthDay.day);
-    });
+        return new Date(year, month, monthDay.day);
+      });
 
     return dates;
   }, [currentMonth, monthAvailability]);
@@ -122,6 +123,11 @@ const Dashboard: React.FC = () => {
     });
   }, [appointments]);
 
+  const nextAppointment = useMemo(() => {
+    return appointments.find(appointment =>
+      isAfter(parseISO(appointment.date), new Date()));
+  }, [appointments]);
+
   return (
     <Container>
       <Header>
@@ -132,7 +138,9 @@ const Dashboard: React.FC = () => {
             <img src={user.avatar_url} alt={user.name} />
             <div>
               <span>Bem-vindo,</span>
-              <strong>{user.name}</strong>
+              <Link to="/profile">
+                <strong>{user.name}</strong>
+              </Link>
             </div>
           </Profile>
 
@@ -151,53 +159,63 @@ const Dashboard: React.FC = () => {
             <span>{selectedWeekDay}</span>
           </p>
 
-          <NextAppointment>
-            <strong>Atendimento a seguir</strong>
-            <div>
-              <img src="https://media-exp1.licdn.com/dms/image/C4D03AQEIo5yV_WiaHQ/profile-displayphoto-shrink_200_200/0/1599415331003?e=1614211200&v=beta&t=3s2q0yVvnWs9cZKdLb_BxquPiPNL6Rp-JBaLF_z0uqE" alt="Gui Munis" />
-              <strong>Gui Munis</strong>
-              <span>
-                <FiClock />
-                08:00
-              </span>
-            </div>
-          </NextAppointment>
+          {isToday(selectedDate) && nextAppointment && (
+            <NextAppointment>
+              <strong>Agendamento a seguir</strong>
+              <div>
+                <img src={nextAppointment.user.avatar_url} alt={nextAppointment.user.name} />
+                <strong>{nextAppointment.user.name}</strong>
+                <span>
+                  <FiClock />
+                  {nextAppointment.hourFormatted}
+                </span>
+              </div>
+            </NextAppointment>
+          )}
 
           <Section>
             <strong>Manhã</strong>
 
+            {morningAppointments.length === 0 && (
+              <p>Nenhum agendamento neste período</p>
+            )}
+
             {morningAppointments.map(appointment => (
               <Appointment key={appointment.id}>
-              <span>
-                <FiClock />
-                {appointment.hourFormatted}
-              </span>
+                <span>
+                  <FiClock />
+                  {appointment.hourFormatted}
+                </span>
 
-              <div>
-                <img src={appointment.user.avatar_url} 
-                alt={appointment.user.name} />
-                <strong>{appointment.user.name}</strong>
-              </div>
-            </Appointment>
+                <div>
+                  <img src={appointment.user.avatar_url}
+                    alt={appointment.user.name} />
+                  <strong>{appointment.user.name}</strong>
+                </div>
+              </Appointment>
             ))}
           </Section>
 
           <Section>
             <strong>Tarde</strong>
 
+            {afternoonAppointments.length === 0 && (
+              <p>Nenhum agendamento neste período</p>
+            )}
+
             {afternoonAppointments.map(appointment => (
               <Appointment key={appointment.id}>
-              <span>
-                <FiClock />
-                {appointment.hourFormatted}
-              </span>
+                <span>
+                  <FiClock />
+                  {appointment.hourFormatted}
+                </span>
 
-              <div>
-                <img src={appointment.user.avatar_url} 
-                alt={appointment.user.name} />
-                <strong>{appointment.user.name}</strong>
-              </div>
-            </Appointment>
+                <div>
+                  <img src={appointment.user.avatar_url}
+                    alt={appointment.user.name} />
+                  <strong>{appointment.user.name}</strong>
+                </div>
+              </Appointment>
             ))}
           </Section>
         </Schedule>
--- web/src/pages/Dashboard/styles.ts
+++ web/src/pages/Dashboard/styles.ts
@@ -55,8 +55,13 @@ export const Profile = styled.div`
     color: #f4ede8;
   }
 
-  strong {
+  a {
+    text-decoration: none;
     color: #ff9000;
+
+    &:hover {
+      opacity: 0.8;
+    }
   }
 `;
 
@@ -160,6 +165,10 @@ export const Section = styled.section`
     display: block;
     padding-bottom: 16px;
     margin-bottom: 16px;
+  }
+
+  > p {
+    color: #999591;
   }
 `;
 

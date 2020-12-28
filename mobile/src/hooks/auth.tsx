2651c04efd29c98202b0d38aeef2bb80e6f70e40
guilherme munis <gmmunis@hotmail.com>
2020-12-28 16:48:59 -0300

    Buscando providers da API

--- mobile/src/hooks/auth.tsx
+++ mobile/src/hooks/auth.tsx
@@ -46,7 +46,11 @@ const AuthProvider: React.FC = ({ children }) => {
         '@GoBarber:user',
       ]);
 
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
 

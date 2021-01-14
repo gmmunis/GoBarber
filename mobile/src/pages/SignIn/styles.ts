fb33c8db143ec5f9c869dbb521bd3f9ea686b51b
guilherme munis <gmmunis@hotmail.com>
2021-01-14 15:12:10 -0300

    fix styled components SignIn e SignOut

--- mobile/src/pages/SignIn/styles.ts
+++ mobile/src/pages/SignIn/styles.ts
@@ -5,7 +5,7 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  padding: 0 30px 80px;
+  padding: 0 30px ${Platform.OS === 'android' ? 190: 40}px;
 `;
 
 export const Title = styled.Text`
